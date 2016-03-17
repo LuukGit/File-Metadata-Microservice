761add6d04587345420287a2ec63486f63cf9a2c
Luuk <luukvoorbrowsen@gmail.com>
2016-03-17 20:23:32 +0000

    FINISHED

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-/*
-  User Story: I can submit a FormData object that includes a file upload.
-  User Story: When I submit something, I will receive the file size in bytes within the JSON response
-  Hint: You may want to use this package: https://www.npmjs.com/package/multer
-*/
 var http = require('http');
 var path = require('path');
 var express = require('express');
@@ -17,10 +12,7 @@ app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.post('/uploads', upload.single("file"), function (req, res) {
   console.log(req.file);
-  res.send({
-    Name: req.file.originalname,
-    Size: req.file.size,
-  });
+  res.send({Size: req.file.size});
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
